90287e2ae838f412de0d1ac9726690d706e14736
Samrat Man Singh <samratmansingh@gmail.com>
2013-12-28 11:14:39 +0545

    Add viterbi fn which gives the probability of the most probable tag sequence.

--- src/pos_tagger/hmm.clj
+++ src/pos_tagger/hmm.clj
@@ -21,15 +21,46 @@
 
 (let [counts (read-brown-counts "brown.counts")]
   (def ngram-count (first counts))
-  (def word-tag-count (second counts)))
+  (def word-tag-count (second counts))
+  (def tag-space (conj (keys word-tag-count) "*")))
 
 (defn transition-probability
   [ngram]
-  (/ (ngram-count ngram)
-     (ngram-count (butlast ngram))))
+  (try (/ (ngram-count ngram)
+          (ngram-count (butlast ngram)))
+       (catch Exception _ 0)))
 
 (defn emission-probability
   "Probability of tag emitting word."
-  [tag word]
-  (/ (get-in word-tag-count [tag word])
-     (apply + (vals (word-tag-count tag)))))
+  [[tag word]]
+  (try (/ (get-in word-tag-count [tag word])
+          (apply + (vals (word-tag-count tag))))
+       (catch Exception _ 0)))
+
+(defn state-seq-probability
+  "Probability of state sequence given observation sequence
+  i.e. P(tags|sentence)"
+  [tag-seq sentence]
+  (let [tag-seq (concat ["*" "*"] tag-seq) ;; * * is the start sequence
+        trigrams (partition 3 1 tag-seq)]
+    (* (apply * (map transition-probability trigrams))
+       (apply * (map emission-probability (map vector
+                                               (drop 2 tag-seq) ;; *'s
+                                               sentence))))))
+
+(defn viterbi
+  [sentence tag posn]
+  (if (= posn 1)
+    (* (emission-probability [tag (first sentence)])
+       (transition-probability ["*" tag]))
+    (* (emission-probability [tag (nth sentence (dec posn))])
+       (apply max (map (fn [prev-tag]
+                   (* (transition-probability [prev-tag tag])
+                      (viterbi sentence prev-tag (dec posn))))
+                       tag-space)))))
+
+(defn viterbi-path
+  [sentence posn]
+  (if (= posn (dec (count sentence)))
+    (apply max-key (fn [tag] (viterbi sentence tag posn)) tag-space)
+    ))
